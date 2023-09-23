5d401328b46ddc9fdd79dec6591c22c80090caf1
KreeDmaste4 <142033862+KreeDmaste4@users.noreply.github.com>
2023-09-23 14:29:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+
+var oclock = +prompt('Введите время')
+
+switch (oclock) {
+    case 1:
+        alert( oclock + ' час ночи ' )
+        break ;
+    case 2:
+        alert( oclock + ' часа ночи ' )
+        break ;
+    case 3:
+        alert( oclock + ' часа ночи ' )
+        break ;
+    case 4:
+        alert( oclock + ' часа ночи ' )
+        break ;
+    case 5:
+        alert( oclock + ' часов ночи ' )
+        break ;
+   case 6:
+    alert( oclock + ' часов ночи ' )
+    break ;
+    
+    case 7:
+        alert( oclock + ' часов утра ' )
+        break ;
+    case 8:
+        alert( oclock + ' часов утра ' )
+        break ;
+   case 9:
+    alert( oclock + ' часов утра' )
+    break ;
+    case 10:
+        alert( oclock + ' часов утра ' )
+        break ;
+        
+        case 11:
+            alert( oclock + ' часов дня ' )
+            break ;
+        case 12:
+            alert( oclock + ' часов дня ' )
+            break ;
+        case 13:
+            alert( 1 + ' час дня ' )
+            break ;
+        case 14:
+            alert( 2 + ' часа дня ' )
+            break ;
+        case 15:
+            alert( 3  + ' часа дня ' )
+            break ;
+       case 16:
+        alert( 4 + ' часа дня ' )
+        break ;   
+        case 17:
+            alert( 5 + ' часов дня ' )
+            break ; 
+            
+            case 18:
+                alert( 6 + ' часов вечера ' )
+                break ;
+            case 19:
+                alert( 7 + ' часов вечера ' )
+                break ;
+            case 20:
+                alert( 8 + ' часов вечера ' )
+                break ;
+            case 21:
+                alert( 9 + ' часов вечера ' )
+                break ;
+            case 22:
+                alert( 10 + ' часов вечера ' )
+                break ;
+           case 23:
+            alert( 11 + ' часов вечера ' )
+            break ;
+}
